3414ab970cd16a340c45412d7295b7a5a5bd9055
Sean Meade <smeade@haymorr.com>
2022-01-24 17:06:43 +0000

    Add Need Help section to home page and add some add function to grab if one help item is clicked.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,11 @@
+var helpItems = document.querySelectorAll(".help");
+
+console.log(helpItems)
+
+helpItems.forEach(helpItem => helpItem.addEventListener('click', (event) => {
+    console.log("hello");
+}))
+
 function expand(){
     document.getElementById('logo-text').style.color = "#B7FDFE";
     document.getElementById('logo-image').style.width = "63px";
