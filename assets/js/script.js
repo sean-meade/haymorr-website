533ca45c14620d85af03ab53e99ebab6d0e0d58e
Sean Meade <smeade@haymorr.com>
2022-01-24 15:04:43 +0000

    Add script.js file, add ids to logo text and logo img, and create a transition of expanding image and text appearing when logo is hovered over.

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+function expand(){
+    document.getElementById('logo-text').style.color = "#B7FDFE";
+    document.getElementById('logo-image').style.width = "63px";
+    
+}
+
+function returnNormal() {
+    document.getElementById('logo-text').style.color = "#533745";
+    document.getElementById('logo-image').style.width = "53px";
+}
+
